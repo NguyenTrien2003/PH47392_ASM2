52e58b98af8499868f2aafa5911c9f3dd278f2f8
NguyenTrien <trienntph41852@fpt.edu.vn>
2024-10-16 03:12:24 +0700

    okkkk

--- ios/ph41852_assignment2Tests/ph41852_assignment2Tests.m
+++ ios/ph41852_assignment2Tests/ph41852_assignment2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ph41852_assignment2Tests : XCTestCase
+@interface CROASMTests : XCTestCase
 
 @end
 
-@implementation ph41852_assignment2Tests
+@implementation CROASMTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
